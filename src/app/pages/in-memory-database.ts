d297bcae7c6a52c16c7231a5e1378698bc6e3ed0
João Kiakumbo <kiakumbo.9@gmail.com>
2019-01-06 17:25:37 +0100

    Adding category model

---
+++ src/app/pages/in-memory-database.ts
@@ -0,0 +1,15 @@
+import {InMemoryDbService} from "angular-in-memory-web-api";
+import { Category } from './categories/shared/category.model';
+
+export class InMemoryDataBase implements InMemoryDbService{
+    createDb(){
+        const categories: Category[] =[
+            { id: 1, name: "Moradia", description: "Pagamentos de contas da Casa" },
+            { id: 2, name: "Saúde", description: "Plano de saúde e remédios" } ,
+            { id: 3, name: "Lazer", description: "Cinema, Parque, Praia etc..." },
+            { id: 4, name: "Salário", description: "Recebimento de salário" },
+            { id: 5, name: "Freelas", description: "Trabalhos como Freelancer" }
+        ];
+        return { categories }
+    }
+}
